9e52722a4c712169949eb831d10f7200074cbe91
Dbatr <bdanis2311@gmail.com>
2024-09-12 18:12:36 +0500

    init empty spring-boot project

---
+++ src/test/java/ru/patterns/PatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
